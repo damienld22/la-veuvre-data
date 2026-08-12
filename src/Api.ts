a70ed491ba941084e3f80295f57817fb0a2aa91e
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2026-08-12 11:00:47 +0200

    feat: periodic fetch

--- src/Api.ts
+++ src/Api.ts
@@ -2,9 +2,11 @@ import { HeightData } from "./types";
 
 const API_DATA_HEIGHT =
   "https://www.vigicrues.gouv.fr/services/observations.json/index.php?CdStationHydro=J708311001&FormatDate=iso";
+const STATIC_DATA_HEIGHT = `${import.meta.env.BASE_URL}data/height.json`;
 
 export async function fetchHeightData(): Promise<HeightData[]> {
-  const data = await (await fetch(API_DATA_HEIGHT)).json();
+  const url = import.meta.env.DEV ? API_DATA_HEIGHT : STATIC_DATA_HEIGHT;
+  const data = await (await fetch(url)).json();
 
   return data.Serie.ObssHydro.map((elt: any) => ({
     value: elt.ResObsHydro,
